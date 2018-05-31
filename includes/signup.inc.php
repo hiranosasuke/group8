80b74bf5828ed16b7928060bf843f9414f60fb2f
Ot Gabaldon Torrents <gabaldonot@gmail.com>
2018-05-31 15:49:59 -0400

    I messed some things up, re-uploading

--- includes/signup.inc.php
+++ includes/signup.inc.php
@@ -3,13 +3,13 @@
 if (isset($_POST['submit']))
 {
 	include_once 'dbh.inc.php';
-	
+
 	$first = mysqli_real_escape_string($conn, $_POST['first']);
 	$last = mysqli_real_escape_string($conn, $_POST['last']);
 	$email = mysqli_real_escape_string($conn, $_POST['email']);
 	$uid = mysqli_real_escape_string($conn, $_POST['uid']);
 	$pwd = mysqli_real_escape_string($conn, $_POST['pwd']);
-	
+
 	// Error handlers
 	// Check for empty fields
 	if (empty($first) || empty($last) || empty($email) || empty($uid) || empty($pwd))
@@ -38,7 +38,7 @@ if (isset($_POST['submit']))
 				$sql = "SELECT * FROM users WHERE user_uid='$uid'";
 				$result = mysqli_query($conn, $sql);
 				$resultCheck = mysqli_num_rows($result);
-				
+
 				if ($resultCheck > 0)
 				{
 					header("Location: ../signup.php?signup=usertaken");
@@ -50,7 +50,7 @@ if (isset($_POST['submit']))
 					$hashedPwd = password_hash($pwd, PASSWORD_DEFAULT);
 					// Insert user into database
 					$sql = "INSERT INTO users (user_first, user_last, user_email, user_uid, user_pwd) VALUES ('$first', '$last', '$email', '$uid', '$hashedPwd');";
-					
+
 					mysqli_query($conn, $sql);
 					header("Location: ../signup.php?signup=success");
 					exit();
@@ -61,6 +61,6 @@ if (isset($_POST['submit']))
 }
 else
 {
-	header("Location: ../signup.php");
+	header("Location: ../signup.php?signup=fail");
 	exit();
 }
---
+++ signup.php
@@ -0,0 +1,68 @@
+<!-- <script src="https://ajax.googleapis.com/ajax/libs/angularjs/1.6.4/angular.min.js"></script> -->
+<!DOCTYPE HTML>
+<html>
+<?php
+  include_once 'header.php';
+ ?>
+
+<body>
+<nav class="navbar navbar-inverse">
+	<div class="container-fluid">
+		<div class="navbar-header">
+			<a class="navbar-brand" href="index.php">Contact <i class="fa fa-user"></i> Manager</a>
+		</div>
+		<div class="collapse navbar-collapse" id="myNavBar">
+
+		</div>
+	</div>
+</nav>
+<div class="container-fluid">
+	<div class="row">
+		<div class="col-md-2 col-sm-2 col-xs-12"></div>
+		<div class="col-md-8 col-sm-8 col-xs-12">
+			<form class="form-container" action="includes/signup.inc.php" method="POST">
+			<h1>Sign Up Form</h1>
+			  <div class="form-group">
+				 <input type="firstname" name="first" class="form-control" id="exampleFirst" placeholder="First Name">
+			  </div>
+			  <div class="form-group">
+				 <input type="lastname" name="last" class="form-control" id="exampleLast" placeholder="Last Name">
+			  </div>
+			  <div class="form-group">
+				 <input type="email" name="email" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp" placeholder="Email">
+			  </div>
+			  <div class="form-group">
+				 <input type="uid" name="uid" class="form-control" id="exampleUser" placeholder="Username">
+			  </div>
+			  <div class="form-group">
+				 <input type="password" name="pwd" class="form-control" id="exampleUser" placeholder="Password">
+			  </div>
+			  <div class="form-check">
+				 <input type="checkbox" class="form-check-input" id="exampleCheck1">
+				 <label class="form-check-label" for="exampleCheck1">Remember Me</label>
+			  </div>
+			  <button type="submit" name="submit" class="btn btn-primary btn-block">Sign Up</button>
+        <!--Response for submit-->
+        <?php
+          if(isset($_GET['signup'])){
+
+            if($_GET['signup']=='success'){
+              echo '<button class = "btn btn-success btn-block disabled"> Sign Up Success!</button>';
+            }else if ($_GET['signup']=='fail'){
+                echo '<button class = "btn btn-danger btn-block disabled"> Sign Up Failed!</button>';
+            }else if($_GET['signup']=='empty'){
+              echo '<button class = "btn btn-warning btn-block disabled"> Fill in All Feilds</button>';
+            }else if($_GET['signup']=='usertaken'){
+              echo '<button class = "btn btn-light btn-block disabled" style="color:red"> Username is Taken </button>';
+            }
+          }
+         ?>
+			</form>
+		</div>
+		<div class="col-md-2 col-sm-2 col-xs-12"></div>
+	</div>
+</div>
+</body>
+
+
+</html>
