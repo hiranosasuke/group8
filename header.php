2b15f575dd98af959df7e9c6bc1b2368c8bd22e5
Ot Gabaldon Torrents <gabaldonot@gmail.com>
2018-05-31 17:02:46 -0400

    Added non copyrighted picture and Updated bootstrap

--- header.php
+++ header.php
@@ -6,10 +6,12 @@
 	<meta charset="utf-8">
 	<meta name="viewport" content="width=device-width, initial-scale=1.0">
 	<!-- Latest compiled and minified CSS-->
-	<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
+	<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
+	<script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.min.js" integrity="sha384-JZR6Spejh4U02d8jOt6vLEHfe/JQGiRRSQQxSfFWpi1MquVdAyjUar5+76PVCmYl" crossorigin="anonymous"></script>
 
 	<!-- jQuery library-->
-	<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
+	<script src="https://code.jquery.com/jquery-3.2.1.slim.min.js" integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN" crossorigin="anonymous"></script>
+<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.9/umd/popper.min.js" integrity="sha384-ApNbgh9B+Y1QKtv3Rn7W3mgPxhU9K/ScQsAP7hUibX39j7fakFPskvXusvfa0b4Q" crossorigin="anonymous"></script>
 
 	<!-- Latest compiled JavaScript -->
 	<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
@@ -18,15 +20,16 @@
   <!-- Font Awesome CDN -->
 	<link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.0.13/css/all.css" integrity="sha384-DNOHZ68U8hZfKXOrtjWvjxusGo9WQnrNx2sqG0tfsghAvtVlRW3tvkXWZh58N9jp" crossorigin="anonymous">
 </head>
-<nav class="navbar navbar-inverse">
-	<div class="container-fluid">
+
+<nav class="navbar navbar-dark">
+	<div class="container-fluid" >
 		<div class="navbar-header">
 			<a class="navbar-brand" href="index.php">Contact <i class="fa fa-user"></i> Manager</a>
 		</div>
 		<div>
 			<?php
 			if(isset($_SESSION['u_id'])){
-				echo '<form style="margin-top:10px" action="includes/logout.inc.php" method="POST">
+				echo '<form class="form-inline my-2 my-lg-0" action="includes/logout.inc.php" method="POST">
 								<button class="btn btn-danger" style="float:right" type="submit" name="submit">Log Out</button>
 							</form>';
 			}
